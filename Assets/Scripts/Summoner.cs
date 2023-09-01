f5613198bd5cfe9db6434ba5d32e3ef358ae14ed
Jörg Kefalas <int02283@uoi.gr>
2023-09-02 00:33:55 +0300

    Program files

--- Assets/Scripts/Summoner.cs
+++ Assets/Scripts/Summoner.cs
@@ -10,14 +10,20 @@ public class Summoner : Enemy {
     public float maxY;
 
     private Vector2 targetPosition;
+
     private Animator anim;
+
     public float stopDistance;
-    public float attackSpeed;
+
     private float attackTime;
+
+    public float attackSpeed;
+
+    public Enemy enemyToSummon;
     public float timeBetweenSummons;
+
     private float summonTime;
-    public Enemy enemytoSummon;
-    public float MeleeAttackSpeed;
+
 
     public override void Start()
     {
@@ -32,55 +38,65 @@ public class Summoner : Enemy {
     private void Update()
     {
         if(player != null) {
+
             if ((Vector2)transform.position != targetPosition)
             {
                 transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
                 anim.SetBool("isRunning", true);
-            }
-            else{
+            } else {
                 anim.SetBool("isRunning", false);
-                if(Time.time >= summonTime)
+
+                if (Time.time >= summonTime)
                 {
                     summonTime = Time.time + timeBetweenSummons;
                     anim.SetTrigger("Summon");
                 }
+
             }
-        if (Vector2.Distance(transform.position, player.position) < stopDistance)
-        {
-            if (Time.time >= attackTime)
+
+            if (Vector2.Distance(transform.position, player.position) <= stopDistance)
             {
-                attackTime = Time.time + timeBetweenAttacks;
-                StartCoroutine(Attack());
-            }        
+                if (Time.time >= attackTime)
+                {
+                    attackTime = Time.time + timeBetweenAttacks;
+                    StartCoroutine(Attack());
+                }
+            }
+
+
+
         }
     }
 
-    void Summon()
-    {
+
+    public void Summon() {
         if (player != null)
         {
-            Instantiate(enemytoSummon, transform.position, transform.rotation);
+            Instantiate(enemyToSummon, transform.position, transform.rotation);
         }
     }
+
 
     IEnumerator Attack()
     {
-        // Inflict damage to the player.
-        player.GetComponent<Player>().TakeDamage(damage);   
+
+        player.GetComponent<Player>().TakeDamage(damage);
+
         Vector2 originalPosition = transform.position;
         Vector2 targetPosition = player.position;
 
         float percent = 0f;
         while (percent <= 1)
         {
-            // Calculate interpolation for smooth attack motion.
+
             percent += Time.deltaTime * attackSpeed;
             float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
-
-            // Move the enemy smoothly between original and target positions.
             transform.position = Vector2.Lerp(originalPosition, targetPosition, interpolation);
             yield return null;
+
         }
+
     }
 
-}}
+
+}
